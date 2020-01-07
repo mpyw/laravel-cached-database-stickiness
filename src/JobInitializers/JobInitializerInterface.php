b73c8508712c49c333d154ce35fe0a47077cc1ec
mpyw <ryosuke_i_628@yahoo.co.jp>
2020-01-07 18:28:32 +0900

    Feat: Add methods for revoking initialize effects on contract

--- src/JobInitializers/JobInitializerInterface.php
+++ src/JobInitializers/JobInitializerInterface.php
@@ -2,6 +2,7 @@
 
 namespace Mpyw\LaravelCachedDatabaseStickiness\JobInitializers;
 
+use Illuminate\Database\ConnectionInterface;
 use Illuminate\Queue\Events\JobProcessing;
 use Mpyw\LaravelCachedDatabaseStickiness\Events\ConnectionCreated;
 
@@ -24,4 +25,16 @@ interface JobInitializerInterface
      * @param \Mpyw\LaravelCachedDatabaseStickiness\Events\ConnectionCreated $connectionCreatedEvent
      */
     public function initializeOnNewConnection(JobProcessing $jobProcessingEvent, ConnectionCreated $connectionCreatedEvent): void;
+
+    /**
+     * Revoke database stickiness state initialization after processing each job.
+     */
+    public function revokeInitializeEffects(): void;
+
+    /**
+     * Avoid revoking stickiness state initialization when DB::recordsHaveBeenModified() called on the Job execution.
+     *
+     * @param \Illuminate\Database\Connection|\Illuminate\Database\ConnectionInterface $connection
+     */
+    public function dontRevokeEffectsOn(ConnectionInterface $connection): void;
 }
